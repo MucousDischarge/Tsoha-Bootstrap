cb428d129e754f0713ebb8dfba627eed4e9e69f3
ez <ezaalto@dhcp-asv-80.helsinki.fi>
2015-05-26 23:15:07 +0300

    Paivitys 2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Kisa (id, nippelitieto) VALUES (1, 'Koivistolan kisat');
+
+INSERT INTO Kilpailija (id, nippelitieto) VALUES (1234, 'Matti Huhtasaari');
+INSERT INTO Kilpailija (id, nippelitieto) VALUES (1235, 'Juhani Ojatalo');
+
+INSERT INTO Valipiste (id, kisa_id) VALUES (12, 1);
+
+INSERT INTO Aika (valipiste_id, kilpailija_id, aika) VALUES (12, 1234, to_timestamp('200612251011131516','YYYYMMDDHHMISSFF'));
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,22 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Kisa(
+  id SERIAL PRIMARY KEY, 
+  nippelitieto varchar(200)
+);
+
+CREATE TABLE Kilpailija(
+  id SERIAL PRIMARY KEY, 
+  nippelitieto varchar(200)
+);
+
+CREATE TABLE Valipiste(
+  id SERIAL PRIMARY KEY, 
+  kisa_id INTEGER REFERENCES Kisa(id)
+);
+
+CREATE TABLE Aika(
+  valipiste_id INTEGER REFERENCES Valipiste(id),
+  kilpailija_id INTEGER REFERENCES Kilpailija(id),
+  aika timestamp
+);

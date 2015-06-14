4ce2bba6b0c57a2b0c4171b95bbc58aed23c2c56
ez <ezaalto@dhcp-asv-80.helsinki.fi>
2015-06-14 09:07:57 +0300

    Paivitys 5

--- app/models/kayttaja.php
+++ app/models/kayttaja.php
@@ -3,18 +3,40 @@
 class Kayttaja extends BaseModel {
 
     public $id, $kayttajanimi, $salasana;
+    
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
 
-    public static function authenticate() {
-        $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE kayttajanimi = :kayttajanimi AND salasana = :kayttajanimi LIMIT 1', array('kayttajanimi' => $kayttajanimi, 'salasana' => $salasana));
-        $query->execute();
+    public static function authenticate($kayttajanimi, $salasana) {
+        $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE kayttajanimi = :kayttajanimi AND salasana = :salasana LIMIT 1', array('kayttajanimi' => $kayttajanimi, 'salasana' => $salasana));
+        $query->execute(array('kayttajanimi' => $kayttajanimi, 'salasana' => $salasana));
         $row = $query->fetch();
         if ($row) {
-            // Käyttäjä löytyi, palautetaan löytynyt käyttäjä oliona
-            return $row;
+            $kayttaja = new Kayttaja(array(
+                'id' => $row['id'],
+                'kayttajanimi' => $row['kayttajanimi'],
+                'salasana' => $row['salasana']
+            ));
+            return $kayttaja;
         } else {
-            // Käyttäjää ei löytynyt, palautetaan null
             return null;
         }
     }
-
+    
+    public static function find($kayttaja_id) {
+        $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE id = :id LIMIT 1');
+        $query->execute(array('id' => $kayttaja_id));
+        $row = $query->fetch();
+        if ($row) {
+            $kayttaja = new Kayttaja(array(
+                'id' => $row['id'],
+                'kayttajanimi' => $row['kayttajanimi'],
+                'salasana' => $row['salasana']
+            ));
+            return $kayttaja;
+        } else {
+            return null;
+        }
+    }
 }
--- lib/base_controller.php
+++ lib/base_controller.php
@@ -3,8 +3,8 @@
 class BaseController {
 
     public static function get_user_logged_in() {
-        if (isset($_SESSION['user'])) {
-            $kayttaja_id = $_SESSION['user'];
+        if (isset($_SESSION['kayttajanimi'])) {
+            $kayttaja_id = $_SESSION['kayttajanimi'];
             $kayttaja = Kayttaja::find($kayttaja_id);
 
             return $kayttaja;
@@ -14,8 +14,9 @@ class BaseController {
     }
 
     public static function check_logged_in() {
-        // Toteuta kirjautumisen tarkistus tähän.
-        // Jos käyttäjä ei ole kirjautunut sisään, ohjaa hänet toiselle sivulle (esim. kirjautumissivulle).
+        if(!(isset($_SESSION['kayttajanimi']))){
+            Redirect::to('/login');
+        }
     }
 
 }
